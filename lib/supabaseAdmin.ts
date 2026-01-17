f708aff9baffe3f33d3d3bcab6ec62383bc5433c
mecyahh <mecyah@opfgrp.com>
2026-01-17 03:10:14 -0500

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,14 +1,7 @@
-// lib/supabaseAdmin.ts
-
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  {
-    auth: {
-      persistSession: false,
-      autoRefreshToken: false,
-    },
-  }
+  { auth: { persistSession: false } }
 )
