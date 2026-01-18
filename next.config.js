1ce284a8613e2598e2bc3db651cf687951f02f03
mecyahh <mecyah@opfgrp.com>
2026-01-18 10:43:16 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+
+  // IMPORTANT:
+  // If you had `output: 'export'` in here, REMOVE IT.
+  // Static export causes "Export encountered errors" + stack overflow with our app pages.
+}
+
+module.exports = nextConfig
